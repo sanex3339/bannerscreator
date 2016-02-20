ff412a1523553bf337fe54350842bc000f6753eb
kachalov <t.kachalov@advmaker.net>
2016-02-20 17:11:14 +0300

    wip

---
+++ resources/assets/typescript/Decorators/ObservableToStorage.ts
@@ -0,0 +1,80 @@
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { ObservableDataService } from "../Services/ObservableDataService/ObservableDataService";
+import * as _ from 'underscore';
+import get = Reflect.get;
+import {UploadedTemplate} from "../Models/UploadedTemplate/UploadedTemplate";
+
+/**
+ * @param storageKey
+ * @param fromObjectHandler
+ * @returns {function(Object, string=): void}
+ * @constructor
+ */
+export function ObservableToStorage (
+    storageKey: string,
+    fromObjectHandler: (data: any) => any = data => data
+) {
+    return function decoratorFactory (target: Object, decoratedPropertyName?: string): void {
+        let key: string = storageKey || decoratedPropertyName,
+            tempProperty: string = `_${decoratedPropertyName}`,
+            setter: Subject<any> = new BehaviorSubject(target[decoratedPropertyName]);
+
+        setter.subscribe((observable: Observable<any>) => {
+            if (!(observable instanceof Observable)) {
+                return;
+            }
+
+            observable.subscribe((data) => {
+                if (!data || !data.length) {
+                    return
+                }
+
+                localStorage.setItem(key, JSON.stringify(data));
+                target[decoratedPropertyName] = Observable.of(data);
+            });
+        });
+
+        createTempProperty(target, tempProperty);
+
+        Object.defineProperty(target, decoratedPropertyName, {
+            get: (): any => {
+                console.log(1);
+
+                try {
+                    let localStorageData: string = JSON.parse(localStorage.getItem(key)),
+                        returnObservable = Observable.of(localStorageData)
+                            .filter((data: any): any => data)
+                            .map(fromObjectHandler);
+
+                    target[tempProperty] = returnObservable;
+
+                    return returnObservable;
+                } catch (e) {}
+
+                return target[tempProperty];
+            },
+            set: (value: any): void => {
+                if (target[tempProperty] == target[decoratedPropertyName]) {
+                    return;
+                }
+
+                target[tempProperty] = Observable.of(value);
+                setter.next(Observable.of(value));
+            }
+        });
+    };
+
+    /**
+     * @param target
+     * @param propertyName
+     */
+    function createTempProperty (target: any, propertyName: string): void {
+        Object.defineProperty(target, propertyName, {
+            enumerable: false,
+            configurable: true,
+            writable: true,
+            value: false
+        });
+
+    }
+}
--- resources/assets/typescript/Services/UploadedTemplatesService/UploadedTemplatesService.ts
+++ resources/assets/typescript/Services/UploadedTemplatesService/UploadedTemplatesService.ts
@@ -1,5 +1,6 @@
 import { Injectable } from 'angular2/core';
 import { Collection } from '../../Interfaces/Collection';
+import { ObservableToStorage } from '../../Decorators/ObservableToStorage';
 import { Observable } from 'rxjs/Observable'
 import { ObservableDataService } from "../ObservableDataService/ObservableDataService";
 import { UploadedTemplate } from '../../Models/UploadedTemplate/UploadedTemplate';
@@ -11,6 +12,28 @@ export class UploadedTemplatesService implements Collection {
      * @type ObservableDataService<UploadedTemplate>
      */
     private observableDataService: ObservableDataService<UploadedTemplate>;
+
+    /**
+     * @type {Observable<UploadedTemplate[]>}
+     */
+    @ObservableToStorage(
+        'uploaded-templates',
+        (templates: UploadedTemplate[]): UploadedTemplate[] => {
+            return templates.map((template: UploadedTemplate): UploadedTemplate => {
+                if (template instanceof UploadedTemplate) {
+                    return template;
+                }
+
+                return new UploadedTemplate(
+                    template['name'],
+                    template['path'],
+                    template['extension'],
+                    template['logo']
+                );
+            })
+        }
+    )
+    private uploadedTemplates: Observable<UploadedTemplate[]> = Observable.of([]);
 
     constructor (
         observableDataService: ObservableDataService<UploadedTemplate>
@@ -35,12 +58,13 @@ export class UploadedTemplatesService implements Collection {
      */
     public set (template: UploadedTemplate): void {
         this.observableDataService.setData(template);
+        this.uploadedTemplates = this.observableDataService.getData();
     }
 
     /**
      * @returns {Observable<T[]>}
      */
     public get (): Observable<UploadedTemplate[]> {
-        return this.observableDataService.getData();
+        return this.uploadedTemplates;
     }
 }
