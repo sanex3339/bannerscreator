5400d1dd0a36b0bb58f67a0786f0b3d9f9ef49e3
sanex3339 <sanex3339@yandex.ru>
2016-02-20 23:29:35 +0300

    Wip

--- resources/assets/typescript/app.ts
+++ resources/assets/typescript/app.ts
@@ -6,18 +6,20 @@ import { ComponentRef, enableProdMode, provide } from 'angular2/core';
 import { LocalStorageService } from './Services/LocalStorageService/LocalStorageService';
 import { ObservableDataService } from "./Services/ObservableDataService/ObservableDataService";
 import { RedirectService } from "./Services/RedirectService/RedirectService";
-import { UploadedTemplatesService } from "./Services/UploadedTemplatesService/UploadedTemplatesService";
 import { ROUTER_PROVIDERS } from 'angular2/router';
+import { UploadedTemplate } from "./Models/UploadedTemplate/UploadedTemplate";
+import { UploadedTemplatesService } from "./Services/UploadedTemplatesService/UploadedTemplatesService";
 
 //enableProdMode();
 
 bootstrap(AppComponent, [
     ApplicationStageService,
     LocalStorageService,
-    ObservableDataService,
     RedirectService,
     ROUTER_PROVIDERS,
-    UploadedTemplatesService
+    provide(UploadedTemplatesService, {
+        useValue: new UploadedTemplatesService(new ObservableDataService<UploadedTemplate>())
+    })
 ]).then((appRef: ComponentRef) => {
     AppInjector(appRef.injector);
 });
