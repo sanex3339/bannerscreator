f294004ad97bed735885e376e03b5121f04e6772
sanex3339 <sanex3339@yandex.ru>
2016-02-21 14:42:59 +0300

    UploadedTemplatesService as extends of ObservableDataOperation

--- resources/assets/typescript/app.ts
+++ resources/assets/typescript/app.ts
@@ -2,7 +2,7 @@ import { AppComponent } from './app.component';
 import { AppInjector } from './AppInjector';
 import { ApplicationStageService } from './Services/ApplicationStageService/ApplicationStageService';
 import { bootstrap } from 'angular2/platform/browser';
-import { ComponentRef, enableProdMode, provide } from 'angular2/core';
+import { ComponentRef, enableProdMode } from 'angular2/core';
 import { LocalStorageService } from './Services/LocalStorageService/LocalStorageService';
 import { RedirectService } from "./Services/RedirectService/RedirectService";
 import { ROUTER_PROVIDERS } from 'angular2/router';
