93f9e5b46fe5e216f50a6a5110ae9955af686c97
kachalov <t.kachalov@advmaker.net>
2016-02-29 16:18:47 +0300

    WIP

--- resources/assets/typescript/AppInjector.ts
+++ resources/assets/typescript/AppInjector.ts
@@ -2,6 +2,11 @@ import { Injector } from 'angular2/core';
 
 let appInjectorRef: Injector;
 
+/**
+ * @param injector
+ * @returns {Injector}
+ * @constructor
+ */
 export const AppInjector = (injector?: Injector): Injector => {
     if (injector) {
         appInjectorRef = injector;
--- resources/assets/typescript/vendor.ts
+++ resources/assets/typescript/vendor.ts
@@ -1,8 +1,6 @@
 ///<reference path="Typings/typings.d.ts"/>
 
 // Polyfills
-//import 'es6-shim';
-//import 'es6-promise';
 import 'es7-reflect-metadata/dist/browser';
 
 import 'zone.js/dist/zone-microtask';
